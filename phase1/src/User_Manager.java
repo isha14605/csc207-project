2ea6ee7e7f85fe7e1e346f5680cdc6a9bd68063d
Tanya Thaker <tanya.thaker@mail.utoronto.ca>
2020-11-10 23:56:54 +0700

    Modified User_Manager

--- phase1/src/User_Manager.java
+++ phase1/src/User_Manager.java
@@ -2,10 +2,18 @@ import java.util.ArrayList;
 
 public class User_Manager {
     protected ArrayList<User> users = new ArrayList<User>();
+    protected ArrayList<String> email = new ArrayList<String>();
 
     protected User_Manager(){}
 
-    protected void add_user(){}
+    protected boolean add_user(String name, String password, String email){
+        if (!this.email.contains(email)){
+            User u1 = new User(name, password, email);
+            users.add(u1);
+            return true;
+        }
+        return false;
+    }
 
     protected boolean verify_login(String email, String password){
         return true;
