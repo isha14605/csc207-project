2a3cbf749e71dec3e4cc2249fa24237d63915f1e
Anushka Saini <anushka.saini@utoronto.ca>
2020-11-14 03:32:41 +0700

    Committed after adding to SignUpSystem, class still incomplete.

--- phase1/src/SignUpSystem.java
+++ phase1/src/SignUpSystem.java
@@ -2,6 +2,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SignUpSystem {
@@ -27,4 +30,23 @@ public class SignUpSystem {
         printWriter.close();
         return true;
     }
+
+    UserManager uM = new UserManager(); // New instance of UserManager
+    EventManager eM = new EventManager(); // New instance of EventManager
+
+    // Method to allow the Attendee to browse the events and decide which ones they want to see.
+    public ArrayList browseTalks(LocalDateTime ldt){
+        ArrayList<Talk> allTalksList = new ArrayList<Talk>();
+        /* Need to get a list of all the events from the event manager (method is not there) and
+        then keep adding each of those events to the list in this method
+         */
+        return allTalksList; // Return the list of events so the Attendee can browse
+    }
+
+    // Method to sign up an Attendee for an Event.
+    public void signUp(Attendee a, Event e){
+        // Sign the Attendee up for the Event
+        uM.signUp(a,e);
+    }
+
 }
