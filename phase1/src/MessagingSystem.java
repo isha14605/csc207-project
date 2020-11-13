da65912985813177679e66e5231177f27e313951
Tanya Thaker <tanya.thaker@mail.utoronto.ca>
2020-11-14 04:07:46 +0700

    Added majority of functionality to MessagingSystem

--- phase1/src/MessagingSystem.java
+++ phase1/src/MessagingSystem.java
@@ -1,4 +1,25 @@
+import java.io.FileInputStream;
+
+import java.util.Scanner;
+
 public class MessagingSystem {
+
+    UserManager um = new UserManager();
+
+    public MessagingSystem(){};
+
+    public String[] read_message() {
+        Scanner scanner = new Scanner(System.in);
+        String[] record;
+        record = scanner.nextLine().split("\n");
+        scanner.close();
+        return record;
+    }
+
+    public boolean send_messages(Messagable from, Messagable to, String message){
+        message = read_message().toString();
+        return um.message(from, to, message);
+    }
 
 
 }
