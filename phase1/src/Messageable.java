b2a0c191b59d5ef775ea223ee9e0ce57bdfb2e15
Isha Sharma <isha.sha14@gmail.com>
2020-11-13 18:25:54 -0800

    renamed Messageable interface

--- phase1/src/Messageable.java
+++ phase1/src/Messageable.java
@@ -1,4 +1,4 @@
-public interface Messagable {
+public interface Messageable {
     char userType();
     void send_message(User who, String message);
     void receive_message(User who, String message);
